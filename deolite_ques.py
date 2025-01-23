ac3bd71822b7626dad924e708223fd7b007cb77c
LightingShark <145226314+LightingShark@users.noreply.github.com>
2025-01-23 21:10:10 +0530

    Add files via upload

---
+++ deolite_ques.py
@@ -0,0 +1,33 @@
+entered_number = int(input())
+
+actual_number_list = [entered_number]
+entered_number = str(entered_number)
+
+new_list_of_number = []
+for each_char in entered_number:
+    new_list_of_number.append(each_char)
+for char in range(len(new_list_of_number)):
+    new_list_of_number[char] = int(new_list_of_number[char])
+new_list_of_number.extend(actual_number_list)   
+
+
+def find_prime_number(new_list_of_number):
+    result = []
+    for i in range(len(new_list_of_number)):
+        number = new_list_of_number[i]
+        counter = 0
+        for j in range ( 1, number + 1 ):
+            if number % j == 0:
+                counter += 1
+        if counter == 2:
+            result.append(number)
+    return result        
+
+result = find_prime_number(new_list_of_number)
+
+if result == []:
+    print("No prime number found")
+else:    
+    for k in range(len(result)):
+        print((result[k]), end = " ")
+    print()
