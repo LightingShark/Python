033b9cc3c3b6d27a2f91de1fe8b21f441c027d9d
LightingShark <145226314+LightingShark@users.noreply.github.com>
2025-01-29 21:37:51 +0530

    Create count_the_occurrrences.py

---
+++ count_the_occurrrences.py
@@ -0,0 +1,6 @@
+nums_list = [5, 10, 20, 35, 5, 50, 20, 100, 200, 10, 150, 100, 100, 20, 20]
+# Write your code here
+
+number = int(input())
+number_of_items_in_the_list = nums_list.count(number)
+print(number_of_items_in_the_list)
